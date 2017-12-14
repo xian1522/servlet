5c73323fc2bfb23bb052a7c0adc92291a5559a0d
xian1522 <xian1522@126.com>
2017-12-14 23:44:24 +0800

    jar包替换

--- src/com/wj/study/uploadFile/UploadServlet.java
+++ src/com/wj/study/uploadFile/UploadServlet.java
@@ -1,11 +1,19 @@
 package com.wj.study.uploadFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 
 public class UploadServlet extends HttpServlet {
@@ -28,7 +36,25 @@ public class UploadServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+	        List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+	        for (FileItem item : items) {
+	            if (item.isFormField()) {
+	                // Process regular form field (input type="text|radio|checkbox|etc", select, etc).
+	                String fieldName = item.getFieldName();
+	                String fieldValue = item.getString();
+	                // ... (do your job here)
+	            } else {
+	                // Process form file field (input type="file").
+	                String fieldName = item.getFieldName();
+	                String fileName = FilenameUtils.getName(item.getName());
+	                InputStream fileContent = item.getInputStream();
+	                // ... (do your job here)
+	            }
+	        }
+	    } catch (FileUploadException e) {
+	        throw new ServletException("Cannot parse multipart request.", e);
+	    }
 	}
 
 }
