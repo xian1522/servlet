8392c848bac06eb56dc68347c92a4edd28dc68c7
xian1522 <xian1522@126.com>
2017-12-15 15:26:38 +0800

    上传附件，读取在界面显示

--- src/com/wj/study/uploadFile/UploadServlet.java
+++ src/com/wj/study/uploadFile/UploadServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
 
 
 public class UploadServlet extends HttpServlet {
@@ -43,6 +44,7 @@ public class UploadServlet extends HttpServlet {
 	                // Process regular form field (input type="text|radio|checkbox|etc", select, etc).
 	                String fieldName = item.getFieldName();
 	                String fieldValue = item.getString();
+	                System.out.println(fieldName+"-"+fieldValue);
 	                // ... (do your job here)
 	            } else {
 	                // Process form file field (input type="file").
@@ -50,11 +52,18 @@ public class UploadServlet extends HttpServlet {
 	                String fileName = FilenameUtils.getName(item.getName());
 	                InputStream fileContent = item.getInputStream();
 	                // ... (do your job here)
+	                String rs = item.getString("UTF-8");
+	                System.out.println(rs);
+	                request.setAttribute("message", rs);
 	            }
 	        }
+	        RequestDispatcher dispatcher = request.getRequestDispatcher("pages/uploadfile/uploadDisplay.jsp");
+	        dispatcher.forward(request, response);
 	    } catch (FileUploadException e) {
 	        throw new ServletException("Cannot parse multipart request.", e);
 	    }
 	}
+	
+	
 
 }
